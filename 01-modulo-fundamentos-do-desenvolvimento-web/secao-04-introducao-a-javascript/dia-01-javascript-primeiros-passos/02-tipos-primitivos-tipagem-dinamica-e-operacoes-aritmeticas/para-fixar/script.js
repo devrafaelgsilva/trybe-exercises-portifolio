5b696fcbd73342a1bc087ebc27cda3e05c5b2cce
devrafaelgsilva <rafaell.gomes101@gmail.com>
2022-11-25 11:52:02 -0300

    Atualizando exercícios

---
+++ 01-modulo-fundamentos-do-desenvolvimento-web/secao-04-introducao-a-javascript/dia-01-javascript-primeiros-passos/02-tipos-primitivos-tipagem-dinamica-e-operacoes-aritmeticas/para-fixar/script.js
@@ -0,0 +1,19 @@
+/* Para fixar: */
+
+/* 1. Crie uma constante chamada base e atribua a ela o valor 5: */
+
+const base = 5;
+
+/* 2. Crie uma constante uma chamada heigth e atribua a ela o valor 8: */
+
+const heigth = 8;
+
+/* 3. Crie uma constante chamada area e atribua a ela o resultado da multiplicação da base pela heigth (Dica: lembre-se de usar sempre o console.log() para imprimir as variáveis e checar os resultados das operações!): */
+
+const area = heigth * base;
+console.log (area);
+
+/* 4. Crie uma constante chamada perimeter e atribua a ela a soma de todos os lados do retângulo: */
+
+const perimeter = base*2 + heigth*2;
+console.log (perimeter);
---
+++ 01-modulo-fundamentos-do-desenvolvimento-web/secao-07-introducao-a-javascript-es6-e-testes-unitarios/02-exercicios-agora-a-pratica/parte-03-organizando-licoes/script.js
@@ -0,0 +1,23 @@
+/* Parte III - Organizando lições */
+
+/* Suponha que você esteja trabalhando em uma escola e precise fazer algumas atualizações no sistema. Para isso, considere o seguinte código: */
+
+const lesson1 = {
+    materia: 'Matemática',
+    numeroEstudantes: 20,
+    professor: 'Maria Clara',
+    turno: 'manhã',
+  };
+  
+  const lesson2 = {
+    materia: 'História',
+    numeroEstudantes: 20,
+    professor: 'Carlos',
+  };
+  
+  const lesson3 = {
+    materia: 'Matemática',
+    numeroEstudantes: 10,
+    professor: 'Maria Clara',
+    turno: 'noite',
+  };
